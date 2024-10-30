49b6b8c7365b56f438f4c01cd436deaf1c5b6999
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:41:48 -0600

    Update Lectura.py

--- Lectura.py
+++ Lectura.py
@@ -52,3 +52,10 @@ while True:
             cv2.putText(frame, 'G0' + str(info[2:]), (xi - 15, yi - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
             print(" El usuario pertenece a la seccion ejecutiva \n"
                   " Numero de Identificacion: G", str(info[2:]))
+
+        if tipo == 83:  # C->99 # S->83
+            # Dibujamos
+            cv2.polylines(frame, [pts], True, (0, 255, 255), 5)
+            cv2.putText(frame, 'S0' + str(info[2:]), (xi - 15, yi - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+            print(" El usuario pertenece a la seccion salud \n"
+                  " Numero de Identificacion: S", str(info[2:]))
