4680ae2f97e092abc5d79b2cb7b6ba83c27bf034
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:43:08 -0600

    Update GENQR_MEJORADO.py

--- GENQR_MEJORADO.py
+++ GENQR_MEJORADO.py
@@ -1,1 +1,11 @@
 
+# Jona163
+# Autor: Jonathan Hernández
+# Fecha: 25 octubre de 2024 
+# Proyecto: VSArtifcial
+# GitHub: https://github.com/Jona163
+
+# Importamos las librerías necesarias
+import pyqrcode
+import png
+from pyqrcode import QRCode
