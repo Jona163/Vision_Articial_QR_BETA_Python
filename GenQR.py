2b87bc602967050b674f4820ffe87d069cefb26a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:40:06 -0600

    Update GenQR.py

--- GenQR.py
+++ GenQR.py
@@ -1,3 +1,9 @@
+# Jona163
+# Autor: Jonathan Hernández
+# Fecha: 25 octubre de 2024 
+# Proyecto: VSArtifcial
+# GitHub: https://github.com/Jona163
+
 # Importamos las librerias
 import pyqrcode
 import png
