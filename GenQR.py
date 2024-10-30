8f29acf40a24f61557e1079dc1748c00cb752f5d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:38:35 -0600

    Update GenQR.py

--- GenQR.py
+++ GenQR.py
@@ -9,3 +9,13 @@ con = 1234
 
 # Generamos los QR
 while con <= 1239:
+
+    #id = str('j') +  str(con)
+    roster = con
+    id = '65' + str(con)
+    # Creamos los QR
+    qr = pyqrcode.create(65 and id, error= 'L')
+    # Guardamos
+    qr.png('A' + str(roster) + '.png', scale = 6)
+    # Aumentamos con
+    con = con + 1
