97f957c045037f8f4bca2f8afdd9770057cebfbe
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:43:33 -0600

    Update GENQR_MEJORADO.py

--- GENQR_MEJORADO.py
+++ GENQR_MEJORADO.py
@@ -12,3 +12,9 @@ from pyqrcode import QRCode
 # Generamos los códigos QR de identificación
 inicio_id = 1234  # ID inicial
 fin_id = 1239     # ID final
+
+# Iteramos para crear y guardar los códigos QR
+for con in range(inicio_id, fin_id + 1):
+    # Creamos el identificador con prefijo '65'
+    id_qr = f'65{con}'
+    
