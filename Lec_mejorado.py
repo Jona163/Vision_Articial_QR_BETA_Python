be920bf4e10bf7c9313df7d931150065c2854eb9
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-30 10:45:12 -0600

    Update Lec_mejorado.py

--- Lec_mejorado.py
+++ Lec_mejorado.py
@@ -8,3 +8,14 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+
+# Configuramos la videocaptura
+cap = cv2.VideoCapture(0)
+
+# Iniciamos el bucle principal
+while True:
+    # Leemos el frame de la cámara
+    ret, frame = cap.read()
+    if not ret:
+        print("Error al capturar la imagen")
+        break
